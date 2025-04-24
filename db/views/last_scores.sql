132c744160580e9a8c0b517a84c245a6a85d7f8c
kingma-sbw <kingma@sbw-media.ch>
2025-04-24 14:31:53 +0200

    update views

--- db/views/last_scores.sql
+++ db/views/last_scores.sql
@@ -1,16 +1,12 @@
 DROP VIEW IF EXISTS `last_scores`;
-
-DELIMITER #
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `last_scores` AS select
-        `board_score`.`id` AS `id`,
-        `board_score`.`board_id` AS `board_id`,
-        `board_score`.`leader_name` AS `leader_name`,
-        `board_score`.`score` AS `score`,
-        `board_score`.`date` AS `date`
-from
-        `board_score`
-order by
-        `board_score`.`date` desc
-limit
-        10#
-# DELIMITER ;
+CREATE ALGORITHM = UNDEFINED
+/*  DEFINER=`leader_user`@`%` */
+SQL SECURITY DEFINER VIEW `last_scores` AS
+select `board_score`.`id` AS `id`,
+    `board_score`.`board_id` AS `board_id`,
+    `board_score`.`leader_name` AS `leader_name`,
+    `board_score`.`score` AS `score`,
+    `board_score`.`date` AS `date`
+from `board_score`
+order by `board_score`.`date` desc
+limit 10;
